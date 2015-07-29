e07944ed8f256fe43b8a84da49ca8af32919b1e1
Isaac Archer <archeri@cat.pdx.edu>
2015-07-28 19:06:44 -0700

    adding slackbot

---
+++ slackbot.rb
@@ -0,0 +1,18 @@
+#! /bin/env ruby
+
+require "slack"
+require "slack-ruby-client"
+
+Slack.configure do |config|
+  config.token = ARGV[0].to_s
+end
+
+client = Slack::Web::Client.new
+
+client.auth_test
+
+client.on :hello do
+  puts 'Successfully connected.'
+end
+
+client.start!
