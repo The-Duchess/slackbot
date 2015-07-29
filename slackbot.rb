2740ae4e1649fd3ea7707310ebff9191cd440ef5
Isaac Archer <archeri@cat.pdx.edu>
2015-07-28 19:23:13 -0700

    adding slackbot

--- slackbot.rb
+++ slackbot.rb
@@ -12,4 +12,13 @@ client.on :hello do
   puts 'Successfully connected.'
 end
 
+client.on :message do |data|
+      case data['text']
+      when 'bot hi' then
+            client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
+      when /^bot/ then
+            client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, what?"
+      end
+end
+
 client.start!
