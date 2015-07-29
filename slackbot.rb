a4987b821fe0a42f1c77c6521f15854bab370a63
Isaac Archer <archeri@cat.pdx.edu>
2015-07-28 19:33:42 -0700

    adding slackbot

--- slackbot.rb
+++ slackbot.rb
@@ -3,6 +3,12 @@
 
 require "slack-ruby-client"
 
+# plugin_s = []
+# plugins = Plugin_manager.new("./plugins")
+# plugin_s.each { |a| plugins.plugin_load(a) }
+# backlog = []
+# admins = []
+
 Slack.configure do |config|
   config.token = ARGV[0].to_s
 end
@@ -14,12 +20,23 @@ client.on :hello do
 end
 
 client.on :message do |data|
+
+      # backlog.push(data)
+
       case data['text']
       when 'husk hi' then
             client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
       when /^husk/ then
             client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, what?"
       end
+
+=begin
+      @plugins.each do |plugin|
+            if data['text'].match(plugin.regex)
+                  client.message channel: data['channel'], text: plugin.sub(data, backlog, admins).to_s
+            end
+      end
+=end
 end
 
 client.start!
